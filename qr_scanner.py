2a72a43145cd8862b8c847abeaf54dd1e156920a
rtm <raultorrecillamoreno@gmail.com>
2022-02-23 21:25:38 +0100

    v.1-BOT

--- qr_scanner.py
+++ qr_scanner.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 # Telegram
+from queue import Empty
 from telegram import Update, InlineKeyboardMarkup, InlineKeyboardButton
 from telegram.ext import Updater, Filters, MessageHandler, CallbackContext
 
@@ -9,13 +10,25 @@ from pyzbar.pyzbar import decode
 
 # System libraries
 import os
+import json
 from os import listdir
 from os.path import isfile, join
 
 from io import BytesIO
 from PIL import Image
+#  decode
+import sys
+import zlib
+import pprint
+import argparse
 
-TOKEN = "TOKEN_FROM_@BOTFATHER"
+import PIL.Image
+import pyzbar.pyzbar
+import base45
+import cbor2
+from cose.messages import CoseMessage 
+
+TOKEN = "5130549589:AAHSV6Hhp7SM68DYI47TQSu7qZ9h0U76dCE"
 
 def decode_qr(update: Update, context: CallbackContext):
 	chat_id = update.message.chat_id
@@ -29,13 +42,42 @@ def decode_qr(update: Update, context: CallbackContext):
 
 	new_file = context.bot.get_file(foto.file_id)
 	new_file.download('qrcode.png')
-
+	print(new_file)
 	try:
-		result = decode(Image.open('qrcode.png'))
-		context.bot.sendMessage(chat_id=chat_id, text=result[0].data.decode("utf-8"))
+		result = decode(PIL.Image.open('qrcode.png'))
+		
+		cert = result[0].data.decode()
+		# print(cert)
+		# certificado = "{""dcc"":" + cert + "}"
+		# certificado = "{"""+f'dcc"":{cert}' + "}"
+		cert = cert.replace("HC1:", "")
+		decoded = base45.b45decode(cert)
+		decompressed = zlib.decompress(decoded)
+		cose = CoseMessage.decode(decompressed)
+		print(json.dumps(cbor2.loads(cose.payload), indent=2))
+
+		b45data = cert.replace("HC1:", "")
+		zlibdata = base45.b45decode(b45data)
+		# print(zlibdata)
+		cbordata = zlib.decompress(zlibdata)
+		# print(cbordata)
+		decoded = cbor2.loads(cbordata)
+		print(len(json.dumps(cbor2.loads(cose.payload), indent=2))>0)
+		# print(result[0].data)  ["-260"]["1"]["nam"]["fn"]
+		if len(json.dumps(cbor2.loads(cose.payload), indent=2))>0 and json.dumps(cbor2.loads(cose.payload), indent=2).__contains__("ci"):
+			context.bot.sendMessage(chat_id=chat_id, text="Su certificado es válido")
+		else:
+			# context.bot.sendMessage(chat_id=chat_id, text=result[0].data.decode("utf-8"))
+			# context.bot.sendMessage(chat_id=chat_id, text=json.dumps(cbor2.loads(cose.payload), indent=2))
+			context.bot.sendMessage(chat_id=chat_id, text="Su certificado no es correcto")
+
 		os.remove("qrcode.png")
 	except Exception as e:
-		context.bot.sendMessage(chat_id=chat_id, text=str(e))
+		context.bot.sendMessage(chat_id=chat_id, text="Su código QR no es correcto")
+
+# def verify():
+
+
 
 def main():
 	updater = Updater(TOKEN, request_kwargs={'read_timeout': 20, 'connect_timeout': 20}, use_context=True)
